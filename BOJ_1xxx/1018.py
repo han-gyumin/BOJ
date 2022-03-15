bc738d34d08e373d1400e535c0f1eb8f06ff8bcf
han-gyumin <90388420+han-gyumin@users.noreply.github.com>
2022-03-15 11:13:06 +0900

    nothing

---
+++ BOJ_1xxx/1018.py
@@ -0,0 +1,6 @@
+a, b = map(int, input().split())
+arr=[]
+for i in range(a):
+    arr.append(input())
+
+print(arr)
---
+++ python programming/0315.py
@@ -0,0 +1,11 @@
+num=123
+print(num)
+print(num+num)
+# variable
+x=10.0
+y=2.5
+z=x*y
+print(z)
+print(type(123))
+print(type(z))
+print(type("hello"))
