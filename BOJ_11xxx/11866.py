4e775706cb6d9b1878cbba8c3f04016e98751f0a
han-gyumin <90388420+han-gyumin@users.noreply.github.com>
2022-03-17 18:41:47 +0900

    Create 11866.py

---
+++ BOJ_11xxx/11866.py
@@ -0,0 +1,22 @@
+# 요세푸스()
+from collections import deque
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+q = deque()
+
+
+for i in range(n):
+    q.append(i + 1)
+
+print("<", end="")
+while len(q) != 0:
+    for i in range(k - 1):
+        q.append(q[0])
+        q.popleft()
+    print(q.popleft(), end="")
+
+    if len(q) != 0:
+        print(", ", end="")
+
+print(">")
