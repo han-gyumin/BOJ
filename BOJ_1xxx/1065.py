e12986f12a0c1b07f79c23d48ac4d41f49618acb
han-gyumin <90388420+han-gyumin@users.noreply.github.com>
2022-04-05 13:17:13 +0900

    Create 1065.py

---
+++ BOJ_1xxx/1065.py
@@ -0,0 +1,16 @@
+num=int(input())
+
+arr=[]
+def hansoo(num):
+    count=0
+    for i in range(1,num+1):
+        if len(str(i))<=2:
+            count+=1
+        elif len(str(i))==3:
+            if (int(str(i)[2])-int(str(i)[1]))==(int(str(i)[1])-int(str(i)[0])):
+                count+=1
+        else:
+            pass
+                
+    print(count)
+hansoo(num)
