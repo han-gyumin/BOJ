d2d67337c5315a5316038ec61a1de57d377c0b38
han-gyumin <90388420+han-gyumin@users.noreply.github.com>
2022-05-07 13:36:08 +0900

    Update 1018.py

--- BOJ_1xxx/1018.py
+++ BOJ_1xxx/1018.py
@@ -1,39 +1,34 @@
 n, m = map(int, input().split())
-arr=[]
-count=0
-count2=[]
-for i in range(m):
-    arr.append(input())
-for i in range(m-7):
-    for j in range(n-7):
+lst=[]
+cnt=[]
+for i in range(n):
+    lst.append([])
+    ip=input()
+    for j in range(m):
+        lst[i].append(ip[j])
+
+
+
+for i in range(m-8+1):
+    for j in range(n-8+1):
+        count=0
         for k in range(8):
-            for a in range(8):
-                if arr[i][j]=='W':
-                    if (i+j)%2==0:
-                        if (a+k)%2==0 and arr[k][a]=='B':
-                            count+=1
-                        elif (a+k)%2==1 and arr[k][a]=='W':
-                            count+=1
-                    if(i+j)%2==1:
-                        if(a+k)%2==0 and arr[k][a]=='W':
-                            count+=1
-                        elif(a+k)%2==1 and arr[k][a]=='B':
-                            count+=1
-                if arr[i][j]=='B':
-                    if (i+j)%2==0:
-                        if (a+k)%2==0 and arr[k][a]=='W':
-                            count+=1
-                        elif (a+k)%2==1 and arr[k][a]=='B':
-                            count+=1
-                    if(i+j)%2==1:
-                        if(a+k)%2==0 and arr[k][a]=='B':
-                            count+=1
-                        elif(a+k)%2==1 and arr[k][a]=='W':
-                            count+=1
+            for l in range(8):
+                if lst[j][i]=='W':
+                    if (l+k+i+j)%2==(i+j)%2 and lst[k+j][l+i]=='B':
+                        count+=1
+                    elif (l+k+i+j)%2!=(i+j)%2 and lst[k+j][l+i]=='W':
+                        count+=1
+                elif lst[j][i]=='B':
+                    if (l+k+i+j)%2==(i+j)%2 and lst[k+j][l+i]=='W':
+                        count+=1
+                    if (l+k+i+j)%2!=(i+j)%2 and lst[k+j][l+i]=='B':
+                        count+=1
         if count>32:
             count=64-count
-        count2.append(count)
-        count=0
-    print(i, j)
-print(count2)
-print(min(count2))
+        cnt.append(count)
+
+
+print(min(cnt))
+
+
