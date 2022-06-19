41d852b71b60a546567619b21d6acd5ef7216989
han-gyumin <90388420+han-gyumin@users.noreply.github.com>
2022-06-19 23:04:25 +0900

    Update 2447.py

--- BOJ_2xxx/2447.py
+++ BOJ_2xxx/2447.py
@@ -1,11 +1,40 @@
-num=int(input())
+character={
+    "name":'기사',
+    'level':12,
+    "items":{
+        "sword":"불꽃의 검",
+        "armor":"풀플레이트"
+    },
+    "skill":["베기","세게 베기","아주 세게 베기"]
+}
 
-def star(n):
-    if n==3:
-        print("***")
-        print("* *")
-        print("***")
-    else:
-        n*star(n/3)
+# characte={"name":'기사','level':12,"items":{"sword":"불꽃의 검","armor":"풀플레이트"},"skill":["베기","세게 베기","아주 세게 베기"]}
 
-star(num)
+# name : 기사
+# level : 12
+# sword : 불꽃의 검
+# armor : 풀플레이트
+# skill : 베기
+# skill : 세게 베기
+# skill : 아주 세게 베기
+
+for i in character:
+    if type(character[i])!=dict and type(character[i])!=list:
+        print(f'{i} : {character[i]}')
+    elif type(character[i])==list:
+        for j in character[i]:
+            print(f'{i} : {j}')
+    elif type(character[i])==dict:
+        for j in character[i]:
+            print(f'{j} : {character[i][j]}')
+
+# your code here
+a=[]
+def multiplication_table(num):
+    for i in range(1,10):
+        print(f'{num} x {i} = {num*i}')
+        a.append(num*i)
+    return a
+
+a = multiplication_table(3)
+print(a)
