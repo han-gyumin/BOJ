4ac82f9c23cdddf48acbf5b52e2af922a7f6da69
han-gyumin <90388420+han-gyumin@users.noreply.github.com>
2022-06-22 16:44:08 +0900

    Create 4949.py

---
+++ BOJ_4xxx/4949.py
@@ -0,0 +1,24 @@
+# 균형잡힌 세상
+import sys
+
+num = int(sys.stdin.readline())
+arr = []
+
+for i in range(num):
+    ps = input()
+    for i in ps:
+        if i == "(":
+            arr.append(i)
+        else:
+            if len(arr) == 0:
+                ans = "NO"
+                arr.append("(")
+                break
+            else:
+                arr.pop()
+    if len(arr) == 0:
+        ans = "YES"
+    else:
+        ans = "NO"
+    print(ans)
+    arr = []
