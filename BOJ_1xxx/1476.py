43a21ab31d8a18353d575cf72a6e83da14ea4828
han-gyumin <90388420+han-gyumin@users.noreply.github.com>
2022-07-01 18:17:53 +0900

    Update 1476.py

--- BOJ_1xxx/1476.py
+++ BOJ_1xxx/1476.py
@@ -8,21 +8,28 @@ lst_m=list(map(lambda x: x , range(1,20)))
 
 
 year=0
+
 while(True):
     year+=1
+    e_count=0
+    s_count=0
+    m_count=0
     if e==s and s==m and m==1:
         break
     if e-1==0:
         e=lst_e[-1]
+        e_count+=1
     if s-1==0:
         s=lst_s[-1]
+        s_count+=1
     if m-1==0:
         m=lst_m[-1]
-    elif e-1!=0:
+        m_count+=1
+    if e-1!=0 and e_count!=1:
         e-=1
-    elif s-1!=0:
+    if s-1!=0 and s_count!=1:
         s-=1
-    elif m-1!=0:
+    if m-1!=0 and m_count!=1:
         m-=1
-    print(year)
+    
 print(year)
