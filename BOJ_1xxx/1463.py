14e0e57521870ef71515da5ea14a27687b83643d
han-gyumin <90388420+han-gyumin@users.noreply.github.com>
2022-07-03 14:04:35 +0900

    Update 1463.py

--- BOJ_1xxx/1463.py
+++ BOJ_1xxx/1463.py
@@ -1,21 +1,11 @@
 # 1로 만들기
-x=int(input())
-count=0
-while(x!=1):
-    if (x-1)%3==0:
-        x-=1
-        count+=1
-        pass
-    elif x%3==0:
-        x/=3
-        count+=1
-        pass
-    elif x%2==0:
-        x/=2
-        count+=1
-        pass
-    else:
-        x-=1
-        count+=1
-        pass
-print(count)
+n = int(input())
+d = [0] * (n + 1)	
+
+for i in range(2, n + 1):
+    d[i] = d[i - 1] + 1
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i // 3] + 1)	           
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i // 2] + 1)
+print(d[n])
