4c0749860a987890750f75f9c16104069d13a319
han-gyumin <90388420+han-gyumin@users.noreply.github.com>
2022-07-16 21:15:08 +0900

    Create 16953.py

---
+++ BOJ_16xxx/16953.py
@@ -0,0 +1,19 @@
+# A->B
+
+a,b=map(int,input().split())
+count=1
+while(True):
+    if b==a:
+        break
+    elif (b%2!=0 and b%10!=1) or b<a:
+        count=-1
+        break
+    else:
+        if b%10==1:
+            b//=10
+            count+=1
+        else:
+            b//=2
+            count+=1
+
+print(count)
