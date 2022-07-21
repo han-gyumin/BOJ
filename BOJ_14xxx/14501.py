6cb2cea6437467397e36a90d25315f1e40b7ef5a
han-gyumin <90388420+han-gyumin@users.noreply.github.com>
2022-07-21 22:38:19 +0900

    Update 14501.py

--- BOJ_14xxx/14501.py
+++ BOJ_14xxx/14501.py
@@ -1,12 +1,17 @@
 # 퇴사
-import sys
-input=sys.stdin.readline
-n=int(input())
-lst_t=[]
-lst_p=[]
+n = int(input())
+t = []
+p = []
+dp = []
 for i in range(n):
-    a,b=map(int,input().split())
-    lst_t.append(a)
-    lst_p.append(b)
-print(lst_t)
-print(lst_p)
+    a, b = map(int, input().split())
+    t.append(a)
+    p.append(b)
+    dp.append(b)
+dp.append(0)
+for i in range(n - 1, -1, -1):
+    if t[i] + i > n:
+        dp[i] = dp[i + 1]
+    else:
+        dp[i] = max(dp[i + 1], p[i] + dp[i + t[i]])
+print(dp[0])
